6f2ed9666a3219d775e54a515f0ed6ffe634be6f
ThiagoMoraesReichert <thiagomoraesreichert@gmail.com>
2025-09-01 22:06:39 -0300

    feat/adding db instances

--- app/hello/routes.py
+++ app/hello/routes.py
@@ -1,10 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from ..models import User, db
 
 hello_bp = Blueprint('hello', __name__)
 
 @hello_bp.route('/')
 def index():
-    return "Hello, World!"
+    usuarios = User.query.all()
+    return render_template('index.html', usuarios=usuarios)
 
 @hello_bp.route('/sobre')
 def about():
